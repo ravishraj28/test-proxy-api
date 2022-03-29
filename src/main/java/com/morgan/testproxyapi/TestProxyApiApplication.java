86dd6f7b291ee67797642a8abd9c06191bc0b5a1
ravishraj28 <ravish4public@gmail.com>
2022-03-29 16:44:00 +0530

    Test

---
+++ src/main/java/com/morgan/testproxyapi/TestProxyApiApplication.java
@@ -0,0 +1,13 @@
+package com.morgan.testproxyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProxyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProxyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morgan/testproxyapi/TestProxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morgan.testproxyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProxyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
